2ff96610edabfe6c18ee0d37ba8a659be27bb5fc
Silvana K <silvanakoharian@gmail.com>
2024-02-20 14:01:05 -0500

    add SQL query STATION table

--- SQL_HackerRank_Station.sql
+++ SQL_HackerRank_Station.sql
@@ -164,5 +164,19 @@ WHERE CITY NOT REGEXP('^[AEIOUaeiou]')
 OR CITY NOT REGEXP('[AEIOUaeiou]$');
 
 ----------------------------------------------------------------------------------
+'''
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+Your result cannot contain duplicates.
+'''
+SELECT DISTINCT CITY
+FROM STATION
+WHERE UPPER(LEFT(CITY,1) NOT IN ('A','E','I','O','U'))
+AND UPPER(RIGHT(CITY,1) NOT IN ('A','E','I','O','U'));
 
+--USING REGEX
 
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT REGEXP('^[AEIOUaeiou]')
+AND CITY NOT REGEXP('[AEIOUaeiou]$');
+-----------------------------------------------------------------------------------
