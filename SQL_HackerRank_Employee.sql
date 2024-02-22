5923cd47eec8d7afa3710e0b841b612e38b430ae
Silvana K <silvanakoharian@gmail.com>
2024-02-22 16:11:07 -0500

    add sql file for Employee table

---
+++ SQL_HackerRank_Employee.sql
@@ -0,0 +1,16 @@
+-- The Employee table containing employee data for a company is described as follows:
+
+-- Column           Type
+-- -------          ----------
+-- employee_id      Integer
+-- name             Integer
+-- months           String
+-- salary           Integer
+
+
+-----------------------------------------------------------
+
+'''
+Write a query that prints a list of employee names 
+(i.e.: the name attribute) from the Employee table in alphabetical order.
+'''
--- SQL_HackerRank_Students.sql
+++ SQL_HackerRank_Students.sql
@@ -19,5 +19,3 @@ SELECT NAME
 FROM STUDENTS
 WHERE MARKS > 75
 ORDER BY RIGHT(NAME,3), ID ASC;
-
------------------------------------------------
