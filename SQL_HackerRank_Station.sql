12820eeba275cd6404b679b49a2eecfb75e1dc99
Silvana K <silvanakoharian@gmail.com>
2024-02-27 14:01:36 -0500

    add sql query using station table,round and min

--- SQL_HackerRank_Station.sql
+++ SQL_HackerRank_Station.sql
@@ -231,4 +231,13 @@ LIMIT 1;
 ----------------------------------------------------------------------------------
 
 '''
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. 
+Round your answer to 4 decimal places.
 '''
+
+SELECT ROUND(MIN(LAT_N),4)
+FROM STATION
+WHERE ROUND(LAT_N,4) > 38.7780;
+
+
+
