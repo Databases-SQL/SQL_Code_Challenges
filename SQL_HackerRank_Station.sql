fdf6bd8f1547949205bc064dc120d89743f5168e
Silvana K <silvanakoharian@gmail.com>
2024-02-26 21:17:09 -0500

    add query station table, using sum and round

--- SQL_HackerRank_Station.sql
+++ SQL_HackerRank_Station.sql
@@ -192,4 +192,15 @@ SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2)
 FROM STATION;
 
 ----------------------------------------------------------------------------------
+'''
+Query the sum of Northern Latitudes (LAT_N) from STATION 
+having values greater than  and less than. 
+Truncate your answer to  decimal places.
+'''
 
+SELECT ROUND(SUM(LAT_N), 4)
+FROM STATION
+WHERE ROUND(LAT_N, 4) > 38.7880
+AND ROUND(LAT_N, 4) < 137.2345;
+
+----------------------------------------------------------------------------------
