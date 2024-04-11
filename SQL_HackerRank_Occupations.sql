65c1d639b0dffc58409922ea731921a5cafbd16e
Silvana K <84741857+sikoh@users.noreply.github.com>
2024-04-11 12:18:56 -0400

    Update SQL_HackerRank_Occupations.sql
    Add description of table Occupations

--- SQL_HackerRank_Occupations.sql
+++ SQL_HackerRank_Occupations.sql
@@ -1,1 +1,12 @@
+-- The OCCUPATIONS table is described as follows:
+
+-- OCCUPATIONS 
+-- Column           Type
+---------------------------
+-- Name             String
+-- Occupation       String
+
+-- Occupation will only contain one of the following values: Doctor, Professor, Singer or Actor.
+
+
 
