dbda2d9aef31709359ec1d8eb936baa6ecbd0c83
Silvana K <84741857+sikoh@users.noreply.github.com>
2024-07-05 15:40:52 -0400

    Update SQL_LeetCode_World.sql

--- LeetCode/SQL_LeetCode_World.sql
+++ LeetCode/SQL_LeetCode_World.sql
@@ -17,7 +17,6 @@ A country is big if:
 
 it has an area of at least three million (i.e., 3000000 km2), or
 it has a population of at least twenty-five million (i.e., 25000000).
-Write a solution to find the name, population, and area of the big countries.
 
 Return the result table in any order.
 
@@ -46,7 +45,8 @@ Output:
 | Algeria     | 37100000   | 2381741 |
 +-------------+------------+---------+
 
-
+-------------------------------------------------------------------
+-- Write a solution to find the name, population, and area of the big countries.
 
 SELECT name, population, area 
 FROM World
