8831ab5a502254c8173e4d60c65eac2d9dea5360
Silvana K <84741857+sikoh@users.noreply.github.com>
2024-07-07 16:48:55 -0400

    Update SQL_LeetCode_Sales_Product.sql

--- LeetCode/SQL_LeetCode_Sales_Product.sql
+++ LeetCode/SQL_LeetCode_Sales_Product.sql
@@ -69,4 +69,9 @@
 
 -------------------------------------------------------------------------------------
 
+SELECT p.product_name, s.year, s.price
+FROM Sales as s
+JOIN Product as p
+ON p.product_id = s.product_id
+
 
